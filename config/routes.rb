0a25b535f120980c50867e64d76aefd2fa2981c9
Jacob <jacobpayne007@gmail.com>
2025-06-03 18:08:10 -0500

    add projects and keywords resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/home"
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
+  root "pages#home"
+  resources :projects
+  resources :keywords
 end
