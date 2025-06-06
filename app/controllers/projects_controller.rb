0ed52393827ca9b93bee4a0cb7d28b7fac10373e
Jacob <jacobpayne007@gmail.com>
2025-06-06 16:42:50 -0500

    include logic to limit entries to projects tables on frontend

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,37 @@
 class ProjectsController < ApplicationController
+    include Paginatable
+
   def index
-    @projects = Project.all
+    @current_page = current_page(3)
+    @per_page = 3
+    @projects = paginate_projects(3)[:data]
+    @total_pages= paginate_projects(3)[:total_pages]
+
+    @current_page_insights = current_page(6)
+    @insights_per_page = 6
+    @projectsInsights = paginate_project_insights[:data]
+    @total_project_pages = paginate_project_insights[:total_pages]
   end
 
   def show
     @projects = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(product_params)
+    if @project.save
+      redirect_to @project
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(project: [ :name, :user_id ])
+    end
 end
