889340cc0cdf48260c24d473addfcf0e6d211e17
Jacob <jacobpayne007@gmail.com>
2025-06-09 19:55:10 -0500

    basic logic for tables - needs to be refactored into helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,13 @@ class PagesController < ApplicationController
   include Paginatable
 
   def home
-    @current_page = current_page(3)
-    keyword_data = paginate_keywords
+    @kw_table_styling = "home_kw_table"
+    @current_page = current_page(10)
+    keyword_data = paginate_keywords(10, nil)
     @keywords = keyword_data[:data]
-    @totalKeywords = keyword_data[:total]
     @total_keyword_pages = keyword_data[:total_pages]
-
-
     @projects = paginate_projects(3)[:data]
     @total_pages= paginate_projects(3)[:total_pages]
-
     @projectsInsights = paginate_project_insights[:data]
     @total_project_pages = paginate_project_insights[:total_pages]
   end
