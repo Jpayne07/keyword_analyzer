e2037608b5c5e81abc0a9209b15f97fc3a8b3263
Jacob <jacobpayne007@gmail.com>
2025-06-09 19:58:27 -0500

    contains methods for id/insights table, id/category table, id/kw table

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,46 @@
 module ProjectsHelper
+  def project_id_insights_table
+    {
+    title: "Insights",
+    columns: [ "#", "Name", "Insight" ],
+    styling: {
+      parent_wrapper: "project_id_insights_table",
+      table_header_styling: "color-red-500",
+      table_scroll_behavior: "overflow-y-scroll max-h-20"
+    },
+    overflow: "overflow-y-scroll max-h-20"
+  }
+  end
+
+  def project_id_category_table
+    {
+    title: nil,
+    columns: [ "#", "Category", "Volume" ],
+    styling: {
+      parent_wrapper: "project_id_category_table",
+      table_header_styling: "text-red-500 text-xl",
+      table_scroll_behavior: "overflow-y-scroll max-h-20"
+    },
+    table_primary_field: "url"
+
+  }
+  end
+
+  def project_id_kw_table
+    {
+    title: "Keywords",
+    columns: [ "#", "Keyword", "Search Volume" ],
+    styling: {
+      parent_wrapper: "project_id_kw_table",
+      table_header_styling: nil,
+      table_scroll_behavior: nil
+    }
+  }
+  end
+  def kw_columns
+    [ "#", "Keyword", "Search Volume" ]
+  end
+  def project_columns
+    [ "#", "Name", "Insight" ]
+  end
 end
