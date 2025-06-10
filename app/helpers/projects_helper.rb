5cc26ebd170620fa1c05cc8f7bafb2dafbe42f5f
Jacob <jacobpayne007@gmail.com>
2025-06-10 18:24:33 -0500

    include hashes for dynamic styling

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,14 +2,25 @@ module ProjectsHelper
   def project_id_insights_table
     {
     title: "Insights",
-    columns: [ "#", "Name", "Insight" ],
+    columns: [ "#", "URL", "SV", "Est Traffic" ],
+    urls: @top_urls,
     styling: {
       parent_wrapper: "project_id_insights_table",
       table_header_styling: "color-red-500",
-      table_scroll_behavior: "overflow-y-scroll max-h-20"
+      table_scroll_behavior: "overflow-y-scroll max-h-20",
+      overflow: "overflow-y-scroll max-h-20",
+      table_body_styling: {
+        row_border: "border border-dashed border-red-500",
+        index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
+        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
+      }
     },
-    overflow: "overflow-y-scroll max-h-20"
-  }
+
+    include: {
+      traffic: true
+    }
+}
   end
 
   def project_id_category_table
@@ -19,9 +30,16 @@ module ProjectsHelper
     styling: {
       parent_wrapper: "project_id_category_table",
       table_header_styling: "text-red-500 text-xl",
-      table_scroll_behavior: "overflow-y-scroll max-h-20"
+      table_scroll_behavior: "overflow-y-scroll max-h-20",
+      table_body_styling: {
+        row_border: "border border-dashed border-red-500",
+        index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
+        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
+      }
     },
     table_primary_field: "url"
+
 
   }
   end
@@ -32,8 +50,15 @@ module ProjectsHelper
     columns: [ "#", "Keyword", "Search Volume" ],
     styling: {
       parent_wrapper: "project_id_kw_table",
+      table_scroll_behavior: nil,
       table_header_styling: nil,
-      table_scroll_behavior: nil
+      table_body_styling: {
+        row_border: "border border-dashed border-red-500",
+        index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
+        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
+      }
+
     }
   }
   end
