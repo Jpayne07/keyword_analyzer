429f16d9b169e508eb24d075984ce34cbc4a47b0
Jacob <jacobpayne007@gmail.com>
2025-06-10 18:23:16 -0500

    moving excess logic out of controller into helper

--- app/controllers/concerns/project_view_data.rb
+++ app/controllers/concerns/project_view_data.rb
@@ -15,6 +15,14 @@ module ProjectViewData
   def project_id_url_table
     @current_page = current_page(10)
     keyword_data = paginate_keywords(10, @project.id)
+    @top_urls = Keyword
+      .where(project_id: @project.id)
+      .group(:url)
+      # !!!url as name is hacky, there is probably a better solution...need to revisit
+      .select("url AS name, SUM(Search_Volume) AS search_volume, SUM(estimated_traffic) AS est_traffic")
+      .order("search_volume DESC")
+      .limit(5)
+
     @keywords = keyword_data[:data]
     @total_keyword_pages = keyword_data[:total_pages]
     projects_data = paginate_projects(3)
