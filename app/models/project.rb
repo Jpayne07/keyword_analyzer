8e8e6206c0fb557afc185f3bf9477e05e02e3354
Jacob <jacobpayne007@gmail.com>
2025-06-14 00:24:25 -0500

    limit projects to 100 for testing, should eventually revert in prod

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,13 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :keywords, dependent: :destroy
+  has_many :ngrams, dependent: :destroy
   validate :projects_count_within_limit, on: :create
-
+  accepts_nested_attributes_for :keywords, allow_destroy: true
   @project_limit=5
 
   def projects_count_within_limit
-    if user.projects.reload.count >= 5
+    if user.projects.reload.count >= 100
     errors.add(:base, "Exceeded projects limit")
     print errors.full_messages
     end
