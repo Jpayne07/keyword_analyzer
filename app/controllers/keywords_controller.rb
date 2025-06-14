f3d4a9432c7a8b88b747ec38285a4e3dfae16d78
Jacob <jacobpayne007@gmail.com>
2025-06-14 00:17:15 -0500

    update to include simple logic

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -2,4 +2,18 @@ class KeywordsController < ApplicationController
   def index
     @keywords = Keyword.all.limit(10)
   end
+  def create
+  @keyword = Keyword.new(keyword_params)
+    if @keyword.save
+      redirect_to @keyword.project, notice: "Keyword created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+
+def keyword_params
+  params.require(:keyword).permit(:name, :search_volume, :url, :keyword_category, :project_id)
+end
 end
