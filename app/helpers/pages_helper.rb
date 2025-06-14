e844e0804d9ed1dcf6e71e2b61b2163c333b32c7
Jacob <jacobpayne007@gmail.com>
2025-06-14 00:22:49 -0500

    add pages logic for data structure and styling. Should move styling to stylesheets for separation of concerns

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,8 +2,37 @@ module PagesHelper
   def home_keyword_table
     {
     title: "Keywords",
-    columns: [ "#", "Keyword", "Volume" ],
-    styling: "home_kw_table"
+    columns: [ "#", "Keyword", "Search Volume" ],
+    styling: {
+      parent_wrapper: "home_kw_table",
+      table_scroll_behavior: "overflow-y-scroll max-h-100",
+      table_header_styling: nil,
+      table_body_styling: {
+        row_border: "border border-dashed border-red-500",
+        index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
+        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
+      }
+
+    }
+  }
+  end
+  def home_projects_table
+    {
+    title: "Keywords",
+    columns: [ "#", "Keyword", "Search Volume" ],
+    styling: {
+      parent_wrapper: "home_projects_table",
+      table_scroll_behavior: "overflow-y-scroll max-h-100",
+      table_header_styling: nil,
+      table_body_styling: {
+        row_border: "border border-dashed border-red-500",
+        index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
+        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
+      }
+
+    }
   }
   end
 end
