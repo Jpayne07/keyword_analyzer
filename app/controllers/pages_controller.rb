4ebcc167852b85e865d87abbdd7f6a52f3e5b416
Jacob <jacobpayne007@gmail.com>
2025-06-14 00:18:15 -0500

    simpler logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   include Paginatable
 
   def home
+    @keywords = Keyword.limit(100).reorder("search_volume DESC")
     @kw_table_styling = "home_kw_table"
+    @projects_table_styling = "home_projects_table"
     @current_page = current_page(10)
-    keyword_data = paginate_keywords(10, nil)
-    @keywords = keyword_data[:data]
-    @total_keyword_pages = keyword_data[:total_pages]
     @projects = paginate_projects(3)[:data]
     @total_pages= paginate_projects(3)[:total_pages]
     @projectsInsights = paginate_project_insights[:data]
