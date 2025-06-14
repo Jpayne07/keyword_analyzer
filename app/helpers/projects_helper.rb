1890e75d6b57188d772a86050df841a39311fa2a
Jacob <jacobpayne007@gmail.com>
2025-06-14 00:23:21 -0500

    need to move styling to css for separation of concerns

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,24 +1,25 @@
 module ProjectsHelper
   def project_id_insights_table
     {
-    title: "Insights",
-    columns: [ "#", "URL", "SV", "Est Traffic" ],
+
+    columns: [ "#", "URL", "SV", "Est Traffic", "KW Count" ],
     urls: @top_urls,
     styling: {
       parent_wrapper: "project_id_insights_table",
-      table_header_styling: "color-red-500",
+      table_header_styling: "text-red-500",
       table_scroll_behavior: "overflow-y-scroll max-h-20",
       overflow: "overflow-y-scroll max-h-20",
       table_body_styling: {
         row_border: "border border-dashed border-red-500",
         index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
-        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        name_styling: "p-0.5 px-1 w-2/5 overflow-hidden truncate whitespace-nowrap",
         keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
       }
     },
 
     include: {
-      traffic: true
+      traffic: true,
+      kw_count: true
     }
 }
   end
@@ -26,15 +27,16 @@ module ProjectsHelper
   def project_id_category_table
     {
     title: nil,
-    columns: [ "#", "Category", "Volume" ],
+    columns: [ "#", "Category", "Volume", "Est. Traffic", "KW Count" ],
+    entries: @top_categories,
     styling: {
       parent_wrapper: "project_id_category_table",
-      table_header_styling: "text-red-500 text-xl",
+      table_header_styling: "text-red-500 text-sm",
       table_scroll_behavior: "overflow-y-scroll max-h-20",
       table_body_styling: {
         row_border: "border border-dashed border-red-500",
         index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
-        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
+        name_styling: "p-0.5 px-1 w-2/5 overflow-hidden truncate whitespace-nowrap",
         keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
       }
     },
@@ -50,7 +52,7 @@ module ProjectsHelper
     columns: [ "#", "Keyword", "Search Volume" ],
     styling: {
       parent_wrapper: "project_id_kw_table",
-      table_scroll_behavior: nil,
+      table_scroll_behavior: "overflow-y-scroll max-h-100",
       table_header_styling: nil,
       table_body_styling: {
         row_border: "border border-dashed border-red-500",
