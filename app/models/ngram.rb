1f9cc40fa8166b5bb7befcf16633c500985fbe7a
Jacob <jacobpayne007@gmail.com>
2025-06-23 15:20:09 -0500

    include logic to filter ngram tables

--- app/models/ngram.rb
+++ app/models/ngram.rb
@@ -1,3 +1,8 @@
 class Ngram < ApplicationRecord
   belongs_to :project
+  def self.search(query, project_id)
+  scope = where(project_id: project_id)
+  scope = scope.where("phrase LIKE ?", "%#{query}%") if query.present?
+  scope
+  end
 end
