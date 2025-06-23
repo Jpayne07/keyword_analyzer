d837dcfeaa6704732fdab298beac57983fc5a6c2
Jacob <jacobpayne007@gmail.com>
2025-06-23 15:10:41 -0500

    updating small phrases

--- app/controllers/concerns/kw_to_ngram.rb
+++ app/controllers/concerns/kw_to_ngram.rb
@@ -1,15 +1,18 @@
 module KwToNgram
   extend ActiveSupport::Concern
 
-  def kw_to_ngram(n_count)
+  def kw_to_ngram
   kw_holder = Hash.new { |hash, key| hash[key] = { count: 0, weighted_frequency: 0 } }
   all_keywords = Keyword.where(project_id: @project.id)
   all_keywords.each do | keyword |
     kw_array = keyword.name.downcase.split(" ")
-    kw_array.each_cons(n_count) do |kw|
-      key = kw.join(" ")
-      kw_holder[key][:count] +=1
-      kw_holder[key][:weighted_frequency] += keyword.search_volume
+    n_count = [ 1, 2, 3, 4 ]
+      n_count.each do |n|
+        kw_array.each_cons(n) do |kw|
+          key = kw.join(" ")
+          kw_holder[key][:count] +=1
+          kw_holder[key][:weighted_frequency] += keyword.search_volume
+          end
       end
     end
     print kw_holder
