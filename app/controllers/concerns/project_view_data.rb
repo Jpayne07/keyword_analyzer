27f4eb7ffb89ab0b0b8595a4fdbf80957369cb3c
Jacob <jacobpayne007@gmail.com>
2025-06-23 15:11:50 -0500

    refactor for readability - include kw_tongram data pass

--- app/controllers/concerns/project_view_data.rb
+++ app/controllers/concerns/project_view_data.rb
@@ -1,40 +1,41 @@
 # app/controllers/concerns/project_view_data.rb
 module ProjectViewData
   extend ActiveSupport::Concern
-
   included do
     before_action :set_project, only: [ :show ]
+    before_action :project_id_tables, only: [ :show ]
   end
-
-
   private
-
   def set_project
     @project = Project.find(params[:id])
   end
 
-  def project_id_url_table
+  def project_id_tables(kw_limit = 100, # allows for custom definition limit based on use
+     ngram_limit = 20,
+      url_limit = 20
+      )
     @current_page = current_page(10)
-    # keyword_data = paginate_keywords(10, @project.id)
     @top_urls = Keyword
       .where(project_id: @project.id)
       .group(:url)
-      # !!!url as name is hacky, there is probably a better solution...need to revisit
-      .select("url AS name, SUM(Search_Volume) AS search_volume, SUM(estimated_traffic) AS est_traffic, COUNT(name) AS kw_count")
+      .select("url AS name, SUM(Search_Volume) AS search_volume, SUM(estimated_traffic) AS estimated_traffic, COUNT(name) AS kw_count")
       .order("search_volume DESC")
-      .limit(5)
-     @top_categories = Keyword
+      .limit(url_limit)
+    @top_categories = Keyword
       .where(project_id: @project.id)
       .group(:keyword_category)
-      .select("keyword_category AS name, SUM(Search_Volume) AS search_volume, SUM(estimated_traffic) AS est_traffic, COUNT(name) AS kw_count")
+      .select("keyword_category AS name, SUM(Search_Volume) AS search_volume, SUM(estimated_traffic) AS estimated_traffic, COUNT(name) AS kw_count")
       .order("search_volume DESC")
       .limit(5)
-
-    @keywords = Keyword.where(project_id: @project.id).limit(100)
-    @projects = Project.where(user: current_user).limit(100)
-    # @total_pages = keyword_data[:total_pages]
-    # @projectsInsights = paginate_project_insights[:data]
-    # @insights_test = Project.all
-    # @total_keyword_pages = keyword_data[:total_pages]
+    @keywords = Keyword
+      .where(project_id: @project.id)
+      .limit(kw_limit)
+    @projects = Project
+      .where(user: current_user)
+      .limit(100)
+    @ngrams = Ngram
+      .where(project: @project)
+      .limit(ngram_limit)
+      .order("weighted_frequency DESC")
   end
 end
