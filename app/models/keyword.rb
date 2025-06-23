38a99b3755b2018f04c5d70701f08f4bfd40cd77
Jacob <jacobpayne007@gmail.com>
2025-06-23 15:19:43 -0500

    include logic to filter keywords when a user filters the tables

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,3 +1,39 @@
 class Keyword < ApplicationRecord
   belongs_to :project
+  before_validation :normalize_url
+  require "csv"
+
+
+  def self.search(query, project_id)
+  scope = where(project_id: project_id)
+  scope = scope.where("name LIKE ?", "%#{query}%") if query.present?
+  scope
+  end
+
+  def self.search_insights(query, project_id)
+  scope = where(project_id: project_id)
+  scope = scope.where("url LIKE ?", "%#{query}%") if query.present?
+  scope.group(:url)
+  .select("url AS name, SUM(search_volume) AS search_volume, SUM(estimated_traffic) AS estimated_traffic, COUNT(name) AS kw_count")
+  .order("search_volume DESC")
+  .limit(50)
+  end
+
+  def normalize_url
+    return unless url.present?
+
+    self.url = url
+      .delete_prefix("https://")
+      .delete_prefix("http://")
+      .delete_prefix("www.")
+  end
+
+    def self.to_csv(keyword)
+    CSV.generate(headers: true) do |csv|
+      csv << [ "Name", "URL", "Search Volume", "Estimated Traffic" ] # headers
+      keyword.each do |k|
+        csv << [ k.name, k.url, k.search_volume, k.estimated_traffic ]
+      end
+    end
+    end
 end
