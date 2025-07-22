391d3dc35e6a117d3604d6d844f58e74bacd6397
Jacob <jacobpayne007@gmail.com>
2025-07-22 18:59:11 -0500

    update counters for the homepage to accurately reflect original vision

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,17 @@ class PagesController < ApplicationController
   include Paginatable
 
   def home
-    @keywords = Keyword.limit(100).reorder("search_volume DESC")
+    @keywords = Keyword.joins(:project).where(projects: { user_id: current_user.id })
     @kw_table_styling = "home_kw_table"
     @projects_table_styling = "home_projects_table"
     @current_page = current_page(10)
-    @projects = paginate_projects(3)[:data]
+    @projects = Project.where(user: current_user)
     @total_pages= paginate_projects(3)[:total_pages]
     @projectsInsights = paginate_project_insights[:data]
     @total_project_pages = paginate_project_insights[:total_pages]
+    @keyword_count = Project.where(user: current_user)
+    .sum { |project| project.keywords.count }
+    @ngram_count = Project.where(user: current_user)
+    .sum { |project| project.ngrams.count }
   end
 end
