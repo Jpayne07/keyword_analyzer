29cfb07b81ce6629adea191136971931f0534a79
Jacob <jacobpayne007@gmail.com>
2025-07-22 19:02:41 -0500

    update home styling

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,7 +5,7 @@ module PagesHelper
     columns: [ "#", "Keyword", "Search Volume" ],
     entry_key: "name".to_sym,
     styling: {
-      parent_wrapper: "project_id_kw_table",
+      parent_wrapper: "bg-white project_id_kw_table ",
       table_scroll_behavior: "overflow-y-scroll max-h-100",
       table_header_styling: "bg-neutral-200",
 
@@ -24,19 +24,25 @@ module PagesHelper
     end
   def home_projects_table
     {
-    title: "Keywords",
-    columns: [ "#", "Keyword", "Search Volume" ],
+    title: "Projects",
+    columns: [ "#", "Title", "Total SV" ],
+    entry_key: "name".to_sym,
     styling: {
-      parent_wrapper: "home_projects_table",
-      table_scroll_behavior: "overflow-y-scroll max-h-100",
-      table_header_styling: nil,
+      parent_wrapper: "shadow-md bg-white text-gray-600 text-left font-medium text-sm flex-1",
+      table_scroll_behavior: "overflow-y-scroll w-full flex-1 max-h-65",
+      overflow: "overflow-y-scroll ",
+      table_header_styling: "bg-neutral-200",
+      table_heading: "text-red-500",
       table_body_styling: {
-        row_border: "border border-dashed border-red-500",
-        index_styling: "border-x border-dashed border-red-500 p-0.5 px-1 text-center h-5",
-        name_styling: "p-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap",
-        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap"
+        row_border: nil,
+        index_styling: "border-r border-dashed border-red-500 p-0.5 px-1 text-center h-5 text-gray-500",
+        name_styling: "pl-0.5 px-1 w-full overflow-hidden truncate whitespace-nowrap text-gray-500",
+        keyword_styling: "p-.5 px-1 overflow-hidden truncate whitespace-nowrap text-gray-500"
       }
 
+    },
+    include: {
+      kw_count: false
     }
   }
   end
