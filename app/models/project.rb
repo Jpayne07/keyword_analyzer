75700ae2c2dd7edd8babc4accf88c46f51b4a9e0
Jacob <jacobpayne007@gmail.com>
2025-07-25 10:37:32 -0500

    updating project limit

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,13 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :keywords, dependent: :destroy
   has_many :ngrams, dependent: :destroy
-  # validates :name, presence: true
+  validates :name, presence: true
   validate :projects_count_within_limit, on: :create
   accepts_nested_attributes_for :keywords, allow_destroy: true
   @project_limit=5
 
   def projects_count_within_limit
-    if user.projects.reload.count >= 100
+    if user.projects.reload.count >= 5
     errors.add(:base, "Exceeded projects limit")
     print errors.full_messages
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-  has_many :projects
+  has_many :projects, dependent: :destroy
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
