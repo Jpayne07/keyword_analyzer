127b389b0423563947c3d5e746728a0ad286f384
Jacob <jacobpayne007@gmail.com>
2025-07-25 10:31:29 -0500

    check for user before viewing entries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :require_login
+   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  def logged_in?
+       !current_user.nil?
+  end
   allow_browser versions: :modern
-  def current_user
-      @current_user ||= User.find_by(id: session[:current_user_id]) if session[:current_user_id]
+def current_user
+  @current_user ||= Current.session&.user
+end
+
+  private
+    def require_login
+        unless logged_in?
+          flash[:error] = "You must be logged in to access this section"
+          redirect_to new_session_url # halts request cycle
+
+        end
+    end
+  def render_not_found
+  render file: Rails.root.join("public/404.html"), status: :not_found, layout: false
   end
 end
