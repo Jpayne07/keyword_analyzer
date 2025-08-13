cbb3e69702f4edbd4374a3a6aab8cd1a82be0f81
Jacob <jacobpayne007@gmail.com>
2025-08-12 21:03:31 -0500

    refactor logic for url_pattern to ensure it fits with the new brand dimension, simplify code

--- app/controllers/concerns/phrase_parser.rb
+++ app/controllers/concerns/phrase_parser.rb
@@ -2,68 +2,88 @@ module PhraseParser
   extend ActiveSupport::Concern
 
   def kw_to_ngram
-  kw_holder = Hash.new { |hash, key| hash[key] = { count: 0, weighted_frequency: 0 } }
+  brand_ngram_map = Hash.new do |hash, brand|
+    hash[brand] = Hash.new { |h, key| h[key] = { count: 0, weighted_frequency: 0 } }
+  end
   all_keywords = Keyword.where(project_id: @project.id)
   all_keywords.each do | keyword |
+    brand = keyword.brand&.downcase&.strip.presence || "unbranded"
     kw_array = keyword.name.downcase.split(" ")
     n_count = [ 1, 2, 3, 4 ]
       n_count.each do |n|
         kw_array.each_cons(n) do |kw|
           key = kw.join(" ")
-          kw_holder[key][:count] +=1
-          kw_holder[key][:weighted_frequency] += keyword.search_volume
-          end
+          holder = brand_ngram_map[brand]
+          holder[key][:count] +=1
+          holder[key][:weighted_frequency] += keyword.search_volume
+        end
       end
     end
-    print kw_holder
-    kw_holder.each do | hash, data |
-      #   Turbo::StreamsChannel.broadcast_prepend_to(
-      #   [ @project, :ngrams ], # becomes "project_5_ngrams"
-      #   target: dom_id(@project, :ngrams),
-      #   partial: "projects/ngram",
-      #   locals: { ngram: hash }
-      # )
-      Ngram.create(phrase: hash, weighted_frequency: data[:weighted_frequency], count: data[:count], project: @project)
+    brand_ngram_map.each do |brand, kw_holder|
+      sorted = kw_holder.sort_by do |_, stats|
+      if stats[:weighted_frequency] == 0
+        [ -1, -stats[:count] ] # make it rank lower than any positive weight
+      else
+        [ -stats[:weighted_frequency], 0 ]
+      end
     end
+
+  cutoff = (sorted.size * 0.1).ceil
+  top_10_percent = sorted.first(cutoff)
+  top_10_percent.each_slice(500) do |chunk|
+  records = chunk.map do |phrase, stats|
+    Ngram.new(
+      phrase: phrase,
+      count: stats[:count],
+      weighted_frequency: stats[:weighted_frequency],
+      project: @project,
+      brand: brand
+    )
   end
 
-  def url_pattern
-  # want to use this down the road
-  # uri = URI.parse(url)
-  # path_segments = uri.path.split("/")    # e.g. ["", "tools-electric", "saws"]
-  # first_folder = path_segments[1]
+    # Import 500 at a time
+    Ngram.import(records, validate: false)
+    end
+  end
+end
 
-  kw_holder = Hash.new { |hash, key| hash[key] = { count: 0 } }
-  all_urls = Keyword.where(project_id: @project.id).pluck(:url).uniq
-  categories_saved = []
-  all_urls.each do | url |
-    first_folder = url[/\.com\/([^\/]+)/, 1]
+def url_pattern
+  brands = Keyword.where(project_id: @project.id).pluck(:brand).uniq
 
-  next unless first_folder  # skip if there's no match
-  url_phrase_array = first_folder
-    .downcase
-    .split(/[-.]/)
-    .reject(&:empty?)
-    url_hash = { url => [] }
-    n_count = [ 1, 2, 3 ]
-      n_count.each do |n|
+  brand_category = []
+
+  brands.each do |brand|
+    kw_holder = Hash.new { |hash, key| hash[key] = { count: 0 } }
+    all_urls = Keyword.where(brand: brand).pluck(:url).uniq
+
+    all_urls.each do |url|
+      first_folder = url[/\.com\/([^\/]+)/, 1]
+      next unless first_folder
+
+      url_phrase_array = first_folder
+                           .downcase
+                           .split(/[-.]/)
+                           .reject(&:empty?)
+                           .reject { |t| t == "a" } # filter 'a' up front
+
+      seen_keys = []
+
+      [ 1, 2, 3 ].each do |n|
         url_phrase_array.each_cons(n) do |kw|
           key = kw.join(" ")
           next if key.length < 3
-          next if key.include?(" a ")
-          next if key.include?("a ")
-          next if key.include?(" a")
-          next if key.include?("/")
-          next if url_hash[url].include?(key)
-          kw_holder[key][:count] +=1
-          url_hash[url] << key
-          end
+          next if seen_keys.include?(key)
+
+          seen_keys << key
+          kw_holder[key][:count] += 1
+        end
       end
-      categories_saved << url_hash
     end
-      return_array = [ categories_saved, kw_holder.sort_by { |_, v| -v[:count] }.to_h.take(10) ]
-      # print "url hash: #{url_hash}"
-      return_array
-    # print kw_holder.sort_by { |_, v| -v[:count] }.to_h.take(10)
+
+    top_50 = kw_holder.sort_by { |_, v| -v[:count] }.first(10).to_h
+    brand_category << { brand => top_50 }
   end
+
+  brand_category
+end
 end
