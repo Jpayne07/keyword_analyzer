2c5547a6dd53faad2763815961658df52ae676f7
Jacob <jacobpayne007@gmail.com>
2025-08-12 21:02:01 -0500

    process in batches of 500 and include brand in the import

--- app/controllers/concerns/import_keywords.rb
+++ app/controllers/concerns/import_keywords.rb
@@ -1,19 +1,39 @@
 module ImportKeywords
   extend ActiveSupport::Concern
+  require "activerecord-import"
+
     def import_keywords_from_csv(project, file)
     require "csv"
     content = file.read.force_encoding("UTF-8")
     content = content.sub("\xEF\xBB\xBF", "").gsub("\r", "").strip
-    csv = CSV.parse(content, headers: true)
-    csv.each do |row|
-      row = row.to_h.transform_keys(&:strip)
-      project.keywords.create!(
-        name: row["keyword"],
-        search_volume: row["search_volume"],
-        url: row["url"],
-        estimated_traffic: row["est_traffic"],
-        keyword_category: row["keyword_category"]
-      )
-    end
+    normalizer = proc do |field|
+  case field.downcase
+  when "volume", "search volume"
+    "search_volume"
+  when "name"
+    "client_name"
+  else
+    field.downcase
+  end
+end
+keywords = []
+
+csv = CSV.parse(content, headers: true, header_converters: normalizer)
+csv.each_slice(500) do |rows|
+  keywords = rows.map do |row|
+    row = row.to_h.transform_keys(&:strip)
+    Keyword.new(
+      url: row["url"],
+      name: row["keyword"],
+      search_volume: row["search_volume"],
+      estimated_traffic: row["est_traffic"],
+      keyword_category: row["keyword_category"],
+      project_id: project.id,
+      brand: row["brand"],
+    )
+  end
+
+  Keyword.import(keywords, validate: true)
+end
     end
 end
