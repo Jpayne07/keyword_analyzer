55819ac1d1a2fbd23b71858a430dfd1019f3fd01
Jacob <jacobpayne007@gmail.com>
2025-08-12 21:09:21 -0500

    fix indentation issue

--- app/controllers/concerns/phrase_parser.rb
+++ app/controllers/concerns/phrase_parser.rb
@@ -31,59 +31,58 @@ module PhraseParser
   cutoff = (sorted.size * 0.1).ceil
   top_10_percent = sorted.first(cutoff)
   top_10_percent.each_slice(500) do |chunk|
-  records = chunk.map do |phrase, stats|
-    Ngram.new(
-      phrase: phrase,
-      count: stats[:count],
-      weighted_frequency: stats[:weighted_frequency],
-      project: @project,
-      brand: brand
-    )
-  end
-
+    records = chunk.map do |phrase, stats|
+      Ngram.new(
+        phrase: phrase,
+        count: stats[:count],
+        weighted_frequency: stats[:weighted_frequency],
+        project: @project,
+        brand: brand
+      )
+    end
     # Import 500 at a time
     Ngram.import(records, validate: false)
     end
   end
-end
 
-def url_pattern
-  brands = Keyword.where(project_id: @project.id).pluck(:brand).uniq
+  def url_pattern
+    brands = Keyword.where(project_id: @project.id).pluck(:brand).uniq
 
-  brand_category = []
+    brand_category = []
 
-  brands.each do |brand|
-    kw_holder = Hash.new { |hash, key| hash[key] = { count: 0 } }
-    all_urls = Keyword.where(brand: brand).pluck(:url).uniq
+    brands.each do |brand|
+      kw_holder = Hash.new { |hash, key| hash[key] = { count: 0 } }
+      all_urls = Keyword.where(brand: brand).pluck(:url).uniq
 
-    all_urls.each do |url|
-      first_folder = url[/\.com\/([^\/]+)/, 1]
-      next unless first_folder
+      all_urls.each do |url|
+        first_folder = url[/\.com\/([^\/]+)/, 1]
+        next unless first_folder
 
-      url_phrase_array = first_folder
-                           .downcase
-                           .split(/[-.]/)
-                           .reject(&:empty?)
-                           .reject { |t| t == "a" } # filter 'a' up front
+        url_phrase_array = first_folder
+                            .downcase
+                            .split(/[-.]/)
+                            .reject(&:empty?)
+                            .reject { |t| t == "a" } # filter 'a' up front
 
-      seen_keys = []
+        seen_keys = []
 
-      [ 1, 2, 3 ].each do |n|
-        url_phrase_array.each_cons(n) do |kw|
-          key = kw.join(" ")
-          next if key.length < 3
-          next if seen_keys.include?(key)
+        [ 1, 2, 3 ].each do |n|
+          url_phrase_array.each_cons(n) do |kw|
+            key = kw.join(" ")
+            next if key.length < 3
+            next if seen_keys.include?(key)
 
-          seen_keys << key
-          kw_holder[key][:count] += 1
+            seen_keys << key
+            kw_holder[key][:count] += 1
+          end
         end
       end
+
+      top_50 = kw_holder.sort_by { |_, v| -v[:count] }.first(10).to_h
+      brand_category << { brand => top_50 }
     end
 
-    top_50 = kw_holder.sort_by { |_, v| -v[:count] }.first(10).to_h
-    brand_category << { brand => top_50 }
+    brand_category
   end
-
-  brand_category
-end
+  end
 end
