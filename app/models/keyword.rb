f6c5cf519d9e9048458017b68ed35309af9ce724
Jacob <jacobpayne007@gmail.com>
2025-09-07 14:34:48 -0500

    including limits for keywords per user and keywords per project

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,13 +1,18 @@
 class Keyword < ApplicationRecord
   belongs_to :project
-  before_validation :normalize_url
-  validates :name, presence: true
+  before_validation :normalize_url, :normalize_category
+  validates :name, presence: true, length: { maximum: 50 }
   validates :search_volume, presence: true
   validates :url, presence: true
   validates :brand, presence: true
+  validate :projects_keyword_count_within_limit, on: :create
+  validate :total_kw_count_within_limit, on: :create
+  has_one :user, through: :project
+
   require "csv"
 
-
+  MAX_KEYWORDS_PER_USER = 150_000
+  MAX_KEYWORDS_PER_PROJECT = 100_000
   def self.search(query, project_id)
   scope = where(project_id: project_id)
   scope = scope.where("name LIKE ?", "%#{query}%") if query.present?
@@ -23,6 +28,19 @@ class Keyword < ApplicationRecord
   .limit(50)
   end
 
+  def projects_keyword_count_within_limit
+    if project.keywords.count >= MAX_KEYWORDS_PER_PROJECT
+      errors.add(:base, "Exceeded keywords limit")
+    end
+  end
+  def total_kw_count_within_limit
+    # Get the total number of keywords across all the user's projects
+    keyword_count = Keyword.where(project_id: user.project_ids).count
+    if keyword_count >= MAX_KEYWORDS_PER_USER
+      errors.add(:base, "Exceeded projects limit")
+    end
+  end
+
   def normalize_url
     return unless url.present?
 
@@ -33,11 +51,15 @@ class Keyword < ApplicationRecord
   end
 
     def self.to_csv(keyword)
-    CSV.generate(headers: true) do |csv|
-      csv << [ "Name", "URL", "Search Volume", "Estimated Traffic", "Brand" ] # headers
-      keyword.each do |k|
-        csv << [ k.name, k.url, k.search_volume, k.estimated_traffic, k.brand ]
+      CSV.generate(headers: true) do |csv|
+        csv << [ "Name", "URL", "Search Volume", "Estimated Traffic", "Brand" ] # headers
+        keyword.each do |k|
+          csv << [ k.name, k.url, k.search_volume, k.estimated_traffic, k.brand ]
+        end
       end
     end
+  private
+    def normalize_category
+      self.keyword_category = "(Blank)" if keyword_category.nil?
     end
 end
