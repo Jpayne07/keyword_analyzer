63e21d2a9ec8f4f4615ad5eeb88293485f7fedb3
Jacob <jacobpayne007@gmail.com>
2025-09-09 17:05:39 -0500

    include new helpers for project creation and session creation; refoldering

--- test/integration/project_creation_test.rb
+++ test/integration/project_creation_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
-require_relative "session_creation_helper"
+require "helpers/session_creation_helper"
+require "helpers/project_category_helper"
 require "csv"
 
 $stdout.sync = true
@@ -7,12 +8,8 @@ class ProjectCreationTest < ActionDispatch::IntegrationTest
   setup do
     sign_in_as(users(:one).email_address, "password")
   end
-  # need to test import keywords first
-  # there are 2 pieces here - the project creation, which only accepts the name and user id
   # then the keywords piece, which will actually hold all of the keyword data.
-  #
   # testing the project creation first, regardless of data
-  #
   test "can see form and add project" do
     assert_generates "/", { controller: "pages", action: "home" }
     get "/projects/new"
@@ -21,31 +18,25 @@ class ProjectCreationTest < ActionDispatch::IntegrationTest
     post projects_url, params: { project: { name: "Project3", user_id: users(:one).id } }
     assert_not Project.find_by(name: "Project3").nil?
   end
-# testing modal popup
-test "upon form submission, modal popup" do
-  # Write a CSV file into tmp/
-  get "/projects/new"
-  assert_dom "h2", "New Project"
-  file_path = Rails.root.join("tmp/test_sample.csv")
-  CSV.open(file_path, "w") do |csv|
-    csv << [ "keyword", "search_volume", "brand" ]
-  end
-  uploaded_file = fixture_file_upload(file_path, "text/csv")
-  # confirming modal doesn't popup before post
-  assert_not_dom "h2", "Category Suggestions"
-  post projects_url, params: { project: { name: "Project3", user_id: users(:one).id, csv_file: uploaded_file } }
+  # testing modal popup
+  test "upon form submission, modal popup" do
+  csv_headers = [ "keyword", "search_volume", "brand", "est_traffic",  "url" ]
+  upload_project(csv_headers)
   assert_dom "h2", "Category Suggestions"
-end
-test "upon form submission, bad headers/data include error" do
-  # bad data makes it in still, import needs to throw an error
-  file_path = Rails.root.join("tmp/test_sample.csv")
-  CSV.open(file_path, "w") do |csv|
-    csv << [ "bad_data" ]
   end
-  uploaded_file = fixture_file_upload(file_path, "text/csv")
-  # confirming modal doesn't popup before post
-  assert_not_dom "h2", "Category Suggestions"
-  post projects_url, params: { project: { name: "Project3", user_id: users(:one).id, csv_file: uploaded_file } }
-  assert_dom "h2", "Category Suggestions"
-end
+  test "upon form submission, bad headers/data include error" do
+    csv_headers = [ "bad data" ]
+    upload_project(csv_headers)
+    assert_response 422
+  end
+  test "upon form submission, test category option selection" do
+    upload_project
+    puts @response.body
+    assert_select "input[type=checkbox][data-name=?]", "articles"
+  end
+  test "upon form submission, test brand selection" do
+    upload_project
+    puts @response.body
+    assert_select "option[value=jacobpaynemarketing]"
+  end
 end
