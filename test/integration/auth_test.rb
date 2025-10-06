97b1da6e12a2b838a1cac6fa913231232c5c64b9
Jacob Payne <jacobpayne007@gmail.com>
2025-10-05 20:12:44 -0500

    Updating import to work

--- test/integration/auth_test.rb
+++ test/integration/auth_test.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require_relative 'session_creation_helper'
+require 'helpers/session_creation_helper'
 
 class LoginFlowTest < ActionDispatch::IntegrationTest
   setup do
+    # signs in with some test information
     sign_in_as(users(:one).email_address, 'password')
   end
   test 'User can log in and redirects to home' do
+    # checks that redirect occurs after login
     assert_redirected_to root_path
     follow_redirect!
   end
